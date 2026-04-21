564143612e0f6e53f0af72ea65ff874574aaf153
AlkindiAkhsan <alkindiakhsan@gmail.com>
2026-04-21 08:36:19 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,42 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2 - Data Manipulation and External Files
+ *   Hari dan Tanggal    : Selasa, 21/04/2026
+ *   Nama (NIM)          : Alkindi Bintang Akhsan (13224022)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Sebuah naskah kuno hasil digitalisasi ditemukan dalam kondisi rusak. Huruf-hurufnya masih terbaca, 
+tetapi beberapa simbol kurung tampak tidak lengkap akibat proses pemindaian yang gagal. Tim 
+filologi digital ingin memulihkan naskah itu dengan menghapus sesedikit mungkin simbol kurung 
+agar struktur teks kembali valid, tanpa mengubah urutan karakter lainnya. 
+Hapus seminimal mungkin karakter kurung dari string agar string menjadi valid.
+ * 
+ */
 
-int main(void) {
+
+ #include <stdio.h>
+ #include <string.h>
+  
+ int main(void) {
+    char str[100];
+    scanf("%s", str);
+    for(int i=0;i<=strlen(str);i++){
+        if(str[i] == "("){
+            if(str[i+2] != ")"){
+                free(str[i]);
+                for(int j=0;j<strlen(str)-i;j++){
+                    str[i] = str[i+1];
+                }
+                }
+            }
+            else if(str[i] == ")"){
+                if(str[i-2] != "("){
+                    free(str[i]);
+                    for(int j=0;j<strlen(str)-i;j++){
+                    str[i] = str[i+1];
+                }
+            }
+        }
+    }
+
     return 0;
-}
+ }
+ 
